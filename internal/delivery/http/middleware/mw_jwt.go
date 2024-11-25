0fc221a71c91791da45bfbcc05a58309e4a256c2
senya <arsikkor12@yandex.ru>
2024-11-25 22:30:42 +0300

    поправил разбор токена

--- internal/delivery/http/middleware/mw_jwt.go
+++ internal/delivery/http/middleware/mw_jwt.go
@@ -4,12 +4,14 @@ import (
 	"fmt"
 	"github.com/LI-SeNyA-vE/Gofermat/internal/service"
 	"net/http"
+	"strings"
 )
 
 func VerificationJWT(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		jwtToken := r.Header.Get("Authorization")
-		if jwtToken != "" {
+		authHeader := r.Header.Get("Authorization")
+		if authHeader != "" && !strings.HasPrefix(authHeader, "Bearer ") {
+			jwtToken := strings.TrimPrefix(authHeader, "Bearer ")
 			_, err := service.VerificationToken(jwtToken)
 			if err != nil {
 				http.Error(w, fmt.Sprintf("невалидный токен, ошибка: %s", err), http.StatusUnauthorized)
