7a900476ea676c84117b6575d6b23550009d86d1
senya <arsikkor12@yandex.ru>
2024-11-26 09:29:05 +0300

    чуть-чуть поправил разбор токена

--- internal/delivery/http/middleware/mw_jwt.go
+++ internal/delivery/http/middleware/mw_jwt.go
@@ -12,7 +12,7 @@ func VerificationJWT(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		authHeader := r.Header.Get("Authorization")
 		global.Logger.Infof("Authorization = %s", authHeader)
-		if authHeader != "" && !strings.HasPrefix(authHeader, "Bearer ") {
+		if authHeader != "" && strings.HasPrefix(authHeader, "Bearer ") {
 			jwtToken := strings.TrimPrefix(authHeader, "Bearer ")
 			global.Logger.Infof("токен = %s", authHeader)
 			_, err := service.VerificationToken(jwtToken)
